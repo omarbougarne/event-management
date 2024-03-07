d470fb27d9d0760673f8fe31cf8be30987386e31
omarbougarne <omarwimu@gmail.com>
2024-03-07 06:44:01 +0100

    some-mods

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\City;
-use App\Models\Country;
-use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
