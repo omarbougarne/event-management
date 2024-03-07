2639c5a64b95680e4729759311ff36c17a4e2e42
omarbougarne <omarwimu@gmail.com>
2024-03-07 06:14:36 +0100

    solved-issue-with-create-method

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\City;
+use App\Models\Country;
+use App\Models\Tag;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\AttendingEventController;
+use App\Http\Controllers\AttentingSystemController;
+use App\Http\Controllers\DeleteCommentController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\EventIndexController;
+use App\Http\Controllers\EventShowController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StoreCommentController;
+use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', WelcomeController::class)->name('welcome');
+Route::get('/e', EventIndexController::class)->name('eventIndex');
+Route::get('/e/{id}', EventShowController::class)->name('eventShow');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/events', EventController::class);
+
+
+    Route::get('/attendind-events', AttendingEventController::class)->name('attendingEvents');
+
+    Route::post('/events-attending/{id}', AttentingSystemController::class)->name('events.attending');
+
+    Route::post('/events/{id}/comments', StoreCommentController::class)->name('events.comments');
+    Route::delete('/events/{id}/comments/{comment}', DeleteCommentController::class)->name('events.comments.destroy');
+});
+
+require __DIR__ . '/auth.php';
